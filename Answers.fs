fbb6f1bc9e62b3f500de19f89da2f755ad8bde41
suo18 <77544834+suo18@users.noreply.github.com>
2021-02-02 15:00:26 +0000

    Update Answers.fs

--- Answers.fs
+++ Answers.fs
@@ -1,16 +1,16 @@
 ﻿module Answers
 
-(* 
+(*
 Q1. How many values does the q1Type type have? int8 is an 8 bit numeric type
 (Q1 is a function that must return the answer)
 *)
 
-type q1Type = 
+type q1Type =
     | House of HasRoof: bool * FloorAreaM2: int8
     | Hovel of HasWalls: bool
     | Hut of IsBamboo: bool * IsWaterProof: bool
 
-let q1() : int = failwith "Not answered"
+let q1 (): int = 3
 
 
 
@@ -22,7 +22,7 @@ Q2. Property-based testing is - return the most correct answer:
 2 = more able to find unexpected corner cases than unit testing
 (Q2 is a function that must return the correct answer)
 *)
-let q2() : int = failwithf "Not answered"
+let q2 (): int = 1
 
 (*
 Q3. The Option type is preferable to using Null pointers because
@@ -40,7 +40,7 @@ C = It documents program behaviour
 (Q3 is a function that must return the correct answer)
 *)
 
-let q3() : int = failwithf "Not answered"
+let q3 (): int = 6
 
 
 (*
@@ -49,14 +49,14 @@ element is the nth element of lst added on the head of the nth element of lsts.
 You may assume that lst and lsts have the same length.
 Do not used indexes or List.item.
 *)
-let q4  (lsts: 'a list list) (lst: 'a list) : 'a list list =
-    failwithf "Not answered"
+let q4 (lsts: 'a list list) (lst: 'a list): 'a list list = failwithf "Not answered"
 
 //-----------------DO NOT CHANGE THIS--------------------------------//
 /// function to use when implementing q4 if you have not correctly answered q4
-let q4UsingIndexes (lsts: 'a list list) (lst: 'a list)  =
+let q4UsingIndexes (lsts: 'a list list) (lst: 'a list) =
     printfn "lst=%A, lsts=%A" lst lsts
-    [0..lst.Length-1]
+
+    [ 0 .. lst.Length - 1 ]
     |> List.map (fun i -> lst.[i] :: lsts.[i])
 //--------------------END OF DO NOT CHANGE---------------------------//
 
@@ -69,9 +69,8 @@ You may assume the matrix is not empty.
 Do not uses indexes or List.item
 HINT: consider List.fold
 *)
-let q5 (lsts: 'a list list) : 'a list list = 
-    failwithf "Not answered"
-    
+let q5 (lsts: 'a list list): 'a list list = failwithf "Not answered"
+
 
 (*
 Q6. Write a function that takes two ordered integer lists (ordered by increasing values),
@@ -80,8 +79,7 @@ Q6. Write a function that takes two ordered integer lists (ordered by increasing
     You may not use array functions.
     You should consider using a recursive function.
 *)
-let q6 (left: int list) (right: int list): int list =
-    failwithf "Not answered"
+let q6 (left: int list) (right: int list): int list = failwithf "Not answered"
 
 
 
@@ -90,11 +88,9 @@ let q6 (left: int list) (right: int list): int list =
 (*
 Q7 return the number of sets in setOfSets that el is an element of
 *)
-let q7 (el: int) (setOfSets: int Set Set) =
-    failwithf "Not answered"
+let q7 (el: int) (setOfSets: int Set Set) = failwithf "Not answered"
 
 (*
 Q8) Given a map and a value, return a Set of all the keys that are mapped to the value.
 *)
-let q8 (map: Map<'a, 'b>) (x: 'b): Set<'a> =
-    failwithf "Not answered"
+let q8 (map: Map<'a, 'b>) (x: 'b): Set<'a> = failwithf "Not answered"
